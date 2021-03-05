7a042ce8ab0206d9b5a0d1dc1e815cabcf4d28d1
Wiito <tminemcraft123@gmail.com>
2021-03-05 11:24:49 -0300

    Updated Modal
    on this, i have updated modaç, added some hover effects, made it responsive, and now the js is fully working

--- js/overlay.js
+++ js/overlay.js
@@ -6,8 +6,8 @@ const thing = document.querySelector('.black-thing');
 
 
 function checkAll () {
-    console.log('chamou');
     if (!hbm_btn.classList.contains('none')) {
+        window.menu = new Boolean(true);
         hbm_btn.classList.add('none')
         cls_btn.classList.remove('none')
         fadeElems.forEach((element) => {
@@ -28,10 +28,20 @@ function checkAll () {
 
 hbm_btn.addEventListener('click', checkAll);
 cls_btn.addEventListener('click', checkAll);
-thing.addEventListener('click', checkAll);
+thing.addEventListener('click', closeOverlay);
+
+//Close clicking on overlay
+function closeOverlay () {
+    //Check if the menu is open
+    if (window.menu) {
+        window.menu = false;
+        checkAll();
+    } else {
+        closeModal();
+    }
+}
 
 //Bookmark
-
 document.querySelector('.text__btns__bookmark').addEventListener('click', () => {
     if (document.getElementById('book').checked){
         document.getElementById('bookmark-txt').innerHTML = "Bookmark";
@@ -43,7 +53,6 @@ document.querySelector('.text__btns__bookmark').addEventListener('click', () => 
 //Modal
 const inputModal = document.querySelectorAll('.modal-input');
 
-
 inputModal.forEach((element) => {
     var id = element.dataset.id;
     var pledge = document.getElementById(`modal-pledge-${id}`);
@@ -51,11 +60,52 @@ inputModal.forEach((element) => {
     element.addEventListener('click', () => {
         hideAll();
         pledge.style.display = "block";
+        document.getElementById(`modal-container-${id}`).style.borderColor = "hsl(176, 72%, 28%)";
     })
 })
 
 function hideAll () {
     inputModal.forEach((element) => {
-        document.getElementById(`modal-pledge-${element.dataset.id}`).style.display = "none";
+        var id = element.dataset.id;
+
+        document.getElementById(`modal-pledge-${id}`).style.display = "none";
+        document.getElementById(`modal-container-${id}`).style.borderColor = "hsl(0, 0%, 48%)";
     })
+}
+
+//Modal Overlay
+
+//Open from buttons
+const modal = document.querySelector('.modal');
+const slctRwrd = document.querySelectorAll('.slct-rwrd');
+
+slctRwrd.forEach((element) => {
+    element.addEventListener('click', () => {
+        var id = element.dataset.id;
+
+        if (!modal.classList.contains('fade-in')) {
+            modal.classList.remove('fade-out');
+            thing.classList.remove('fade-out');
+
+            modal.classList.add('fade-in');
+            thing.classList.add('fade-in');
+
+            document.querySelector(`#modal-input-${id}`).checked = true;
+            document.getElementById(`modal-pledge-${id}`).style.display = "block";
+            document.getElementById(`modal-container-${id}`).style.borderColor = "hsl(176, 72%, 28%)";
+        }
+    })
+})
+
+//Close Modal
+document.getElementById('close-modal').addEventListener('click', closeModal);
+
+function closeModal () {
+    hideAll();
+
+    modal.classList.remove('fade-in');
+    thing.classList.remove('fade-in');
+
+    modal.classList.add('fade-out');
+    thing.classList.add('fade-out');
 }
